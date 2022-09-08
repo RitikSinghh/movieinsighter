7c5efddb90b9e87ab9cf08ab48ba605e5ef94e07
Ritik <ritik1050@gmail.com>
2022-09-09 00:39:08 +0530

    moviesite

---
+++ src/Search.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+const Search = () => {
+  const { query, setQuery, isError } = useGlobalContext();
+  return (
+    <>
+      <section className="search-section">
+        <h2>Search Your Favourite Movie</h2>
+        <form action="#" onSubmit={(event) => event.preventDefault()}>
+          <div>
+            <input
+              type="text"
+              placeholder="search here"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+          </div>
+        </form>
+        <div className="card-error">
+          <p>{isError.show && isError.msg}</p>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Search;

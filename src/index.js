977e7a0a1cb91e0bb46e25f621920b51acf09aac
Ritik <ritik1050@gmail.com>
2022-09-09 05:49:00 +0530

    Error fixes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AppProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/movieinsighter">
       <App />
     </BrowserRouter>
   </AppProvider>

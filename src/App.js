4a2fb1117f58786ab817335ca9505307c4c973a0
Ritik <ritik1050@gmail.com>
2022-09-09 05:57:53 +0530

    final time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route ,Navigate} from "react-router-dom";
 import Home from "./Home";
 import SingleMovie from "./SingleMovie";
+
 
 const App = () => {
   return (
